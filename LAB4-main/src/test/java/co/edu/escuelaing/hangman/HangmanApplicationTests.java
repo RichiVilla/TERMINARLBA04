08096bf38c7199edd04fa8ad5be433d2a6df80ce
RichiPichi <ricardovillamizar66@gmail.com>
2024-02-26 23:17:51 -0500

    aaa

--- LAB4-main/src/test/java/co/edu/escuelaing/hangman/HangmanApplicationTests.java
+++ LAB4-main/src/test/java/co/edu/escuelaing/hangman/HangmanApplicationTests.java
@@ -2,12 +2,32 @@ package co.edu.escuelaing.hangman;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class HangmanApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	 private GameScore bonusScore = new BonusScore();
+
+@Test
+    void testBonusScore_CorrectCount3_IncorrectCount1_Returns25() {
+
+        int score = bonusScore.calculateScore(3, 1);
+        assertEquals(25, score);
+    }
+
+    @Test
+    void testBonusScore_CorrectCount0_IncorrectCount0_Returns0() {
+        int score = bonusScore.calculateScore(0, 0);
+        assertEquals(0, score);
+    }
+
+    @Test
+    void testBonusScore_CorrectCountNegative_IncorrectCountPositive_ThrowsException() {
+        assertThrows(modelException.class, () -> {
+            bonusScore.calculateScore(-1, 1);
+        });
+    }
+
 
 }
